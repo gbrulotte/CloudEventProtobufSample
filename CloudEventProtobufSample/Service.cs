809ee83792f93dc3f439c30a12f82b17db83de18
Guillaume Brulotte <gbrulotte@GENETEC.COM>
2023-03-03 15:43:01 -0500

    CloudEvent proto not building

---
+++ CloudEventProtobufSample/Service.cs
@@ -0,0 +1,27 @@
+﻿using CloudNative.CloudEvents.Protobuf;
+using CloudNative.CloudEvents.V1;
+using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+
+namespace CloudEventProtobufSample
+{
+    public class Service : SampleAppGrpcService.SampleAppGrpcServiceBase
+    {
+        public override async Task<Empty> SendEvents(IAsyncStreamReader<CloudEvent> requestStream, ServerCallContext context)
+        {
+            await foreach (var proto in requestStream.ReadAllAsync(context.CancellationToken))
+            {
+                try
+                {
+                    var formatter = new ProtobufEventFormatter();
+                    CloudNative.CloudEvents.CloudEvent cloudEvent = formatter.ConvertFromProto(proto, null);
+                }
+                catch (Exception e)
+                {
+                }
+            }
+
+            return new Empty();
+        }
+    }
+}
